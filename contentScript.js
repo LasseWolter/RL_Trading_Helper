b647248c2a2c0ba47f0475cabf2f0491f58069e0
Lasse Wolter <wolter.lasse@gmail.com>
2020-12-22 20:26:42 +0100

    First version of SaveSearch-Feature
    + Basic flow works, saveBtn to save to storage, sidebar to recall
    BUT
    - All names are set to test
    - no deletion possible
    - sidebar permanent at the moment

--- contentScript.js
+++ contentScript.js
@@ -18,3 +18,61 @@ chrome.runtime.onMessage.addListener(function (request) {
         }
     }
 });
+
+let sidebar = document.createElement('div');
+sidebar.setAttribute('id', 'saved-searches-div');
+let heading = document.createElement('h4');
+heading.innerText = 'Saved Searches';
+sidebar.appendChild(heading);
+
+let list = document.createElement('ul');
+sidebar.appendChild(list);
+document.body.appendChild(sidebar)
+
+let searches = [];
+chrome.storage.sync.get({savedSearches: []}, function (result) {
+    searches = result.savedSearches;
+    for (let search of searches) {
+        let item = document.createElement('li');
+        let itemLink = document.createElement('a');
+        itemLink.href = search.link;
+        itemLink.innerText = search.name;
+        item.appendChild(itemLink);
+        list.appendChild(item);
+    }
+})
+
+
+
+let applyDiv = document.querySelector("div.rlg-trade-filter-content > form > div:nth-child(3) > div.col-1-5");
+// Change 1/5 width of one button into 2 buttons with width 1/8 and 1/12
+// Explanation: 1/5(5/25) is roughly equal to 1/12+1/8 (5/24), so the space will be used well
+applyDiv.classList.add('col-1-8'); // to make width 1/8
+let saveDiv = applyDiv.cloneNode(true);  // deep clone to also clone the btn within the div
+saveDiv.style.width = "calc(100%/12)" // to make width 1/12
+
+let saveBtn = saveDiv.firstElementChild;
+saveBtn.innerText = 'Save';
+saveBtn.href = 'google.com';
+saveBtn.onclick = saveSearch;
+
+saveDiv.appendChild(saveBtn);
+applyDiv.parentNode.appendChild(saveDiv);
+
+function saveSearch(e) {
+    e.preventDefault();
+    let search = {}
+    search.name = 'test';
+    search.link = window.location.href;
+
+    // Idea for following code taken from https://stackoverflow.com/questions/16605706/store-an-array-with-chrome-storage-local
+    // By passing an object you can define default values e.g.: []
+    chrome.storage.sync.get({savedSearches: []}, function (result) {
+        let savedSearches = result.savedSearches;
+        savedSearches.push(search);
+        chrome.storage.sync.set({savedSearches: savedSearches}, () => {
+            console.log('saved');
+            alert('saved');
+        })
+    });
+}
